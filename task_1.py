37e94ef75c46a672c4c8269253357c61a0f5de83
Виктория Слука <103218667+ViktoriyaSluka@users.noreply.github.com>
2022-05-30 17:52:50 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -1,8 +1,25 @@
-def print_type():
-    print(type(15 * 3))
-    print(type(15 / 3))
-    print(type(15 // 2))
-    print(type(15 ** 2))
+'''
+Написать функцию num_translate(), переводящую числительные от 0 до 10 c английского на
+русский язык. Например:
+#>>> num_translate("one")
+"один"
+#>>> num_translate("eight")
+"восемь"
+Если перевод сделать невозможно, вернуть None. Подумайте, как и где лучше хранить
+информацию, необходимую для перевода: какой тип данных выбрать, в теле функции или
+снаружи.
+'''
 
-if __name__ == '__main__':
-    print_type()
+
+def num_translate():
+    translater = input("Введите число от 0 - 10 на английском: ")
+    num_translate = {"zero": "ноль", "one": "один", "two": "два", "three": "три", "four": "четыре", "five": "пять",
+                     "six": "шесть", "seven": "семь", "eight": "восемь", "nine": "девять", "ten": "десять", }
+    for key, valye in num_translate.items():
+        if translater == key:
+            print(valye)
+    if translater not in num_translate:
+        print("None")
+
+if __name__ == "__main__":
+    num_translate()
